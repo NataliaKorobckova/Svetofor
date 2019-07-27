14781aae2f11941d2417e498f13666b68fd1b3cb
BGSSTR <49820965+BGSSTR@users.noreply.github.com>
2019-07-28 02:55:35 +0300

    Svetofor-game

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1564270524,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/exit-sheet0.png",
+		"images/info-sheet0.png",
+		"images/menu-sheet0.png",
+		"images/play-sheet0.png",
+		"images/construct-sheet0.png",
+		"images/figma-sheet0.png",
+		"images/google-sheet0.png",
+		"images/back-sheet0.png",
+		"images/background-sheet0.png",
+		"images/fullscreen-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
